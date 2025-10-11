eabbf0c0ea02dec8332415c4cc43c92a7e38d59c
root <root@apm.hitronhub.home>
2025-10-11 17:55:06 +0000

    updating the nodejs example

--- app/nodejs/index.js
+++ app/nodejs/index.js
@@ -8,28 +8,38 @@ var apm = require('elastic-apm-node').start({
   //environment: 'production'
 });
 // vars
-const express = require('express');
+const express = require('express')
 const app = express()
 const port = 3030
 // app
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-app.get('/path/:params1', (req, res) => {
-  res.send('Path ' + req.params.params1)
-  apm.setTransactionName('/path/'+req.params.params1)
+app.get('/error/automatic', (req, res) => {
+  // bad code generates error
+  res.send(nonDeclaredVariable)
 })
-app.get('/demo', (req, res) => {
-  res.send('Welcome Demo')
-  apm.setCustomContext({'apm': {'alpha': 'beta computer'}});
-})
-app.get('/error', (req, res) => {
-  const span = apm.startSpan('This is a label')
-  res.send('Capture Error!')
-  const err = new Error('Trigger Error!')
+app.get('/error/manual/:params1', (req, res) => {
+  const msg = 'Manual Error: ' + req.params.params1;
+  res.send(msg)
+  const err = new Error(msg)
   apm.captureError(err)
-  span.end()
 })
+app.get('/span/:params1', (req, res) => {
+  res.send('Testing Span')
+  const timeout = 5000;
+  const span = apm.startSpan('Testing a span, will wait ' + timeout + 'ms')
+  setTimeout(()=>span.end(), timeout);
+})
+app.get('/context/:params1', (req, res) => {
+  res.send('Welcome Demo')
+  apm.setCustomContext({'apm': {'alpha': 'beta computer'}})
+})
+app.get('/transaction-name/:params1', (req, res) => {
+  res.send('Transaction Name: ' + req.params.params1)
+  apm.setTransactionName('Path: /transaction-name/'+req.params.params1)
+})
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
